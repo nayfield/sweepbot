88a70a72e1073376a4cc0be948c5c15a8a08860f
rod <rod@nayfield.com>
2020-10-23 14:11:45 -0400

    Initial commit 2

---
+++ config/example-slackcred.py
@@ -0,0 +1,21 @@
+
+# A classic bot token - can get RTM API.
+TOKEN = 'xoxb-123456'
+
+# The bot ID - ignore your own messages
+SUID = 'UAAAAAAA'
+
+# Channel bot posts to
+OUT_CHAN = 'C0FDSGFDGDF'
+
+# Channels bot listens on
+IN_CHANS = ['C0144SFSD']
+
+# Emojis to react on, ony if from user in mods
+EMOJIS = ['large_blue_circle', 'bridge_at_night']
+MODS = ['UJKLDSAFJD','JFDSKLJFK']
+
+
+# Phrases to react on
+PHRASES = ['good night', 'goodnight', 'sign off', 'signing off', 'have to run']
+
